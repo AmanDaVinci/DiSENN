b2e526d27b230c00bf638ff5684de61282fe41f3
amandavinci <amanthevinci@gmail.com>
2020-02-07 12:22:33 +0100

    add master config to manage configs better

---
+++ configs/master_config.py
@@ -0,0 +1,10 @@
+""" Master Configuration
+
+The master configuration dict will be imported by other configs
+and updated with the new information defined for each config
+"""
+
+
+CONFIG = {
+    "data": "mnist"
+}
