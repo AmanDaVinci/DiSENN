39ebb398e72ca481350cdc9512b88fbb50c4dc90
amandavinci <amanthevinci@gmail.com>
2020-02-19 17:59:49 +0100

    add configs

--- configs/master_config.py
+++ configs/master_config.py
@@ -4,7 +4,12 @@ The master configuration dict will be imported by other configs
 and updated with the new information defined for each config
 """
 
-
 CONFIG = {
-    "data": "mnist"
+    "seed": 42,
+    "debug": True,
+    "device": 'cpu',
+    "train": True,
+    "download": False,
+    "valid_freq": 2,
+    "save_freq": 2
 }
