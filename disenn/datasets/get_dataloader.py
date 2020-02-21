e5d17c105268102a5c67b60ae77cb95289d1fe61
amandavinci <amanthevinci@gmail.com>
2020-02-21 09:45:56 +0100

    pin memory to gpu for better gpu usage

--- disenn/datasets/get_dataloader.py
+++ disenn/datasets/get_dataloader.py
@@ -17,12 +17,12 @@ def get_dataloader(config):
     """
     assert config["data"] in ["celeba"], "supports CelebA dataset only"
     path = "data/"+config["data"]
-
+    pin_memory = True if config['device'] is 'gpu' else False
     if config['data'] == 'celeba':
         train_dataset = CelebA(split="train", data_path=path, download=config['download'], target=config["target"])
         valid_dataset = CelebA(split="valid", data_path=path, download=False, target=config["target"])
-        train_dl = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True)
-        valid_dl = DataLoader(valid_dataset, batch_size=config["batch_size"], shuffle=False)
+        train_dl = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True, pin_memory=pin_memory)
+        valid_dl = DataLoader(valid_dataset, batch_size=config["batch_size"], shuffle=False, pin_memory=pin_memory)
         return train_dl, valid_dl
     else:
         return None
