04c4c636899bcabb54c96e2ca6bb6bd640e81eef
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-27 09:43:32 +0000

    add new configs

---
+++ configs/celeba_beta0-5.py
@@ -0,0 +1,23 @@
+from .master_config import CONFIG
+
+config = {
+    "exp_name": 'celeba_beta0-5',
+    "epochs": 5,
+    "data": 'celeba',
+    "target": 'Male',
+    "conceptizer": 'VaeConceptizer',
+    "num_concepts": 10,
+    "beta": 0,
+    "parameterizer": 'ConvParameterizer',
+    "num_classes": 2,
+    "robustness_reg": 1e-4,
+    "aggregator": 'SumAggregator',
+    "concept_loss": 'bvae_loss',
+    "robustness_loss": 'celeba_robustness_loss',
+    "learning_rate": 3e-4,
+    "batch_size": 128,
+    "pretrain_epochs": 0,
+    "pre-beta": 1
+}
+
+CONFIG.update(config)
